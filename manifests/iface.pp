186f544b963b52eaacf469d655740724992d00f1
root <root@scovery.jaggi.lan>
2012-08-12 11:33:31 +0200

    Cleanup interface name overwrite mechanism

--- manifests/iface.pp
+++ manifests/iface.pp
@@ -1,15 +1,9 @@
-define interfaces::iface ( $family, $method, $options=[], $auto=0, $ifname="" ) {
-	if $ifname == "" {
-		$_ifname = $name
-	} else {
-		$_ifname = $ifname
-	}
-
+define interfaces::iface ( $family, $method, $options=[], $auto=0, $ifname=$name ) {
 	if $auto == 1 {
-		interfaces::auto { "${_ifname}": }
+		interfaces::auto { "${ifname}": }
 	}
 
-	$str = "iface ${_ifname} ${family} ${method}\n	<%= options.join('\n	') %>\n\n"
+	$str = "iface ${ifname} ${family} ${method}\n	<%= options.join('\n	') %>\n\n"
 	concat::fragment{"interfaces::iface_${name}":
 		target => '/etc/network/interfaces',
 		content => inline_template($str),
