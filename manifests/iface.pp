b1031b3e8fa3d33c4be724d8bd1886bda65ebacf
root <root@scovery.jaggi.lan>
2012-08-12 11:39:10 +0200

    Puppet style cleanup: use 2-spaces indentation

--- manifests/iface.pp
+++ manifests/iface.pp
@@ -1,11 +1,11 @@
 define interfaces::iface ( $family, $method, $options=[], $auto=0, $ifname=$name ) {
-	if $auto == 1 {
-		interfaces::auto { "${ifname}": }
-	}
+  if $auto == 1 {
+    interfaces::auto { "${ifname}": }
+  }
 
-	$str = "iface ${ifname} ${family} ${method}\n	<%= options.join('\n	') %>\n\n"
-	concat::fragment{"interfaces::iface_${name}":
-		target => '/etc/network/interfaces',
-		content => inline_template($str),
-	}
+  $str = "iface ${ifname} ${family} ${method}\n	<%= options.join('\n	') %>\n\n"
+  concat::fragment{"interfaces::iface_${name}":
+    target => '/etc/network/interfaces',
+    content => inline_template($str),
+  }
 }
