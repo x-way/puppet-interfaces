705d0e1186080dc439c72a2a7fc98d79757b6fa9
Andreas Jaggi <x-way@waterwave.ch>
2012-08-14 16:10:32 +0200

    Add initial rspec-puppet test infrastructure

--- Rakefile
+++ Rakefile
@@ -1,12 +1,17 @@
 require 'rake'
 
 begin
+  require 'rspec/core/rake_task'
   require 'puppet-lint/tasks/puppet-lint'
 rescue LoadError
   require 'rubygems'
   retry
 end
 
-task :test => [:lint]
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.pattern = 'spec/*/*_spec.rb'
+end
+
+task :test => [:spec, :lint]
 
 task :default => :test
---
+++ spec/defines/iface_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'interfaces::iface' do
+  let(:title) { 'lo' }
+  let(:params) { { :family => 'inet', :method => 'loopback', :auto => 1 } }
+
+  it {
+    should contain_interfaces__auto('lo')
+    should contain_concat__fragment('interfaces::iface_lo').with_target('/etc/network/interfaces')
+    should contain_concat__fragment('interfaces::iface_lo').with_content('iface lo inet loopback\n\t\n\n')
+  }
+end
