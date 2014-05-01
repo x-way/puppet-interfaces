6b19b4619eecb915a892a9e18e83a9d078f3c5e5
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2014-05-01 11:35:15 +0200

    only use coveralls if ruby >= 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require 'coveralls'
-Coveralls.wear!
+if RUBY_VERSION.to_f >= 1.9
+  require 'coveralls'
+  Coveralls.wear!
+end
 
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
