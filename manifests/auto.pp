46281d77aa7093b880d2a0f97528f4feaaf39823
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2014-07-27 13:13:47 +0200

    Add $order parameter (fixes issue #2)
    This allows to pass an $order parameter to all definitions.
    If the $order parameter is not explicitely defined, it is automatically
    choosen such thath the configuration for the 'lo' interfaces comes first.

--- manifests/auto.pp
+++ manifests/auto.pp
@@ -1,6 +1,14 @@
-define interfaces::auto ( ) {
+define interfaces::auto ( $order='UNSET' ) {
+  $order_real = $order ? {
+    'UNSET' => $name ? {
+      'lo'    => 06,
+      default => 07,
+    },
+    default => $order
+  }
   concat::fragment{"interfaces::auto_${name}":
     target  => '/etc/network/interfaces',
     content => "auto ${name}\n\n",
+    order   => $order_real,
   }
 }
--- manifests/mapping.pp
+++ manifests/mapping.pp
@@ -1,6 +1,11 @@
-define interfaces::mapping ( $script, $maps ) {
+define interfaces::mapping ( $script, $maps, $order='UNSET' ) {
+  $order_real = $order ? {
+    'UNSET' => 03,
+    default => $order,
+  }
   concat::fragment{"interfaces::mapping_${name}":
     target  => '/etc/network/interfaces',
     content => inline_template("mapping <%= @name %>\n\tscript <%= @script %>\n\t<%= @maps.join('\n\t') %>\n\n"),
+    order   => $order_real,
   }
 }
